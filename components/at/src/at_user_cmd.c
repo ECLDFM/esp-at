cd0db49a6a37bc5b5f924ec073baec85410daaef
ECLDFM <40174286+ECLDFM@users.noreply.github.com>
2023-05-30 08:02:21 +0200

    printf("%d""\r\n", (uint32_t)(time));

--- components/at/src/at_user_cmd.c
+++ components/at/src/at_user_cmd.c
@@ -451,7 +451,7 @@ void at_set_mcu_state_if_sleep(at_sleep_mode_t mode)
     case AT_MIN_MODEM_SLEEP:
         //esp_at_port_active_write_data((uint8_t *)"\r\nAT_MIN_MODEM_SLEEP\r\n",strlen("\r\nAT_MIN_MODEM_SLEEP\r\n"));
         printf("Timer: ");
-        printf("%" PRIx64 "\r\n", time);
+        printf("%d""\r\n", (uint32_t)(time));
         break;
     case AT_LIGHT_SLEEP:
         //esp_at_port_active_write_data((uint8_t *)"\r\nAT_LIGHT_SLEEP\r\n",strlen("\r\nAT_LIGHT_SLEEP\r\n"));
